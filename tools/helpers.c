0cf2ed67ba0fd5986dc9fc211a1eeef3eb50b4c4
snowclipsed <harrybishnoi@gmail.com>
2024-07-03 17:49:02 -0400

    added some helpers for creating 2d matrices

--- tools/helpers.c
+++ tools/helpers.c
@@ -36,3 +36,46 @@ float* create_array_ones(size_t n){
 
 }
 
+float* create_matrix_random(int rows, int cols, float max, float min){
+    float* array = (float*)malloc(cols * rows * sizeof(float));
+    if (array == NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+
+    for(int j = 0; j<rows; j++){
+        for(int i=0; i<cols; i++){
+            float random_value = min + ((float)rand() / RAND_MAX) * (max - min);
+            array[i * cols + j] = random_value;
+        }
+    }
+
+    return array;
+}
+
+float* create_matrix_value(int rows, int cols, float value=1.0f){
+    float* array = (float*)malloc(cols * rows * sizeof(float));
+    if (array == NULL){
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(1);
+    }
+
+    for(int j = 0; j<rows; j++){
+        for(int i=0; i<cols; i++){
+            array[i * cols + j] = value;
+        }
+    }
+
+    return array;
+}
+
+
+void print_2d_array( float* array, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            printf("%.2f\t", array[i * cols + j]);
+        }
+        printf("\n");
+    }
+}
+
